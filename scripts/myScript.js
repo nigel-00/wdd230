b17fb0a7ec71e4671dc202927dfdcab823b06d26
nigel-00 <sangweni1820@gmail.com>
2022-09-17 13:06:39 -0600

    Initial commit: Portal Page Assignment.

---
+++ scripts/myScript.js
@@ -0,0 +1,10 @@
+//get the current full date
+const todayDate = new Date();
+//get the specific current year from the full date
+const fullYear = todayDate.getFullYear();
+//concatinate the full year with the copyright sign 
+document.querySelector('#fullYear').textContent = fullYear;
+// get the last modified date and time
+const dateTime = document.lastModified;
+// create a template literal to combine the text and date and time
+document.querySelector('#lastMod').textContent  = `Last Updated: ${dateTime}`;
