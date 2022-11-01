94f55f509b2e97fc0f263029972cd52cf1bab8d6
Melokuhle Nigel Dube <101907993+nigel-00@users.noreply.github.com>
2022-11-01 04:23:43 -0600

    Table build

--- chamber/js/main.js
+++ chamber/js/main.js
@@ -63,3 +63,36 @@ if (dayNumber ==  1 || dayNumber == 2) {
   }
 
 
+// local storage manipulation
+const todayDays = document.querySelector(".today");
+//start date for first visit
+const startDate = new Date();
+
+let currDate = new Date();
+//division to find the number of days
+const msInDay = 1000 * 60 * 60 * 24;
+
+if (startDate !== 0){
+  let difference = Math.round((currDate - startDate)/msInDay);
+
+  
+}
+// initialize display elements
+const startD = document.querySelector(".today");
+const visitsDisplay = document.querySelector(".visits");
+
+// get the stored value in localStorage
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
+
+// determine if this is the first visit or display the number of visits.
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit!`;
+}
+
+// increment the number of visits.
+numVisits++;
+// store the new number of visits value
+localStorage.setItem("visits-ls", numVisits);
+
