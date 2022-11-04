6e7a7a21484fd2a898ffd08502803b5a9b68fa24
Melokuhle Nigel Dube <101907993+nigel-00@users.noreply.github.com>
2022-11-04 04:45:14 -0600

    edit main

--- chamber/js/main.js
+++ chamber/js/main.js
@@ -32,22 +32,6 @@ function toggleMenu(){
 const x = document.getElementById('hamburgerBtn');
 x.onclick = toggleMenu;
 
-// image couresel
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 3000); // Change image every 2 seconds
-}
-
 // create a message pop up 
 //get a date 
 const today = new Date();
