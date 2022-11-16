37e1d8314c47c117783e20754ff13f8d5b456439
Melokuhle Nigel Dube <101907993+nigel-00@users.noreply.github.com>
2022-11-16 08:00:36 -0700

    fix directory page

---
+++ chamber/js/weather.js
@@ -0,0 +1,31 @@
+//ADD the key and change units to imperial
+const apiURL = "//api.openweathermap.org/data/2.5/weather?id=879431&appid=a7163999040e65fca8574bedafc98106&units=imperial"
+
+//Go fetch it and then wait for a response.
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((weatherInfo) => {
+    //Once it comes back, display it to the console.
+    console.log(weatherInfo);
+    
+  document.getElementById('place').innerHTML = weatherInfo.name;  
+  document.getElementById('temp').innerHTML = weatherInfo.main.temp; 
+  document.getElementById('speed').innerHTML = weatherInfo.wind.speed; 
+  const captionDesc = document.querySelector('figcaption');
+
+
+  const iconcode = weatherInfo.weather[0].icon;
+  const icon_path = "//openweathermap.org/img/w/" +iconcode + ".png";
+  console.log(icon_path);
+
+  const desc = weatherInfo.weather[0].description;
+  const firstsub = desc.substring(0, desc.indexOf(' '));
+  const firstSubUpper  = firstsub.charAt(0).toUpperCase() + firstsub.slice(1);
+  const secsub = desc.substring(desc.indexOf(' ') + 1);
+  const secSubUpper  = secsub.charAt(0).toUpperCase() + secsub.slice(1);
+  const combined = `${firstSubUpper} ${secSubUpper}`;
+  captionDesc.textContent = combined;
+
+  document.getElementById('weather_icon').src = icon_path; 
+
+ }); //end of "then" fat arrow function
